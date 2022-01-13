303d98bb6e6e6113a6c983d72dbd2ea283878d5d
shirajuki <shirajuki00@gmail.com>
2022-01-13 20:32:04 +0100

    feat: adds boilerplate for game window

--- src/main/java/com/shirajuki/breakoutlibgdx/App.java
+++ src/main/java/com/shirajuki/breakoutlibgdx/App.java
@@ -1,13 +1,17 @@
 package com.shirajuki.breakoutlibgdx;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class App {
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("Tutorial");
+
+        config.setWindowedMode(960, 640);
+        // config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        new Lwjgl3Application(new AppController(), config);
     }
 }
---
+++ src/main/java/com/shirajuki/breakoutlibgdx/AppController.java
@@ -0,0 +1,35 @@
+package com.shirajuki.breakoutlibgdx;
+
+import java.util.ArrayList;
+import java.util.Random;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class AppController extends Game {
+  ShapeRenderer shape;
+  ArrayList<Ball> balls = new ArrayList<>();
+  Random r = new Random();
+
+  @Override
+  public void create() {
+    shape = new ShapeRenderer();
+    for (int i = 0; i < 10; i++) {
+      balls.add(new Ball(r.nextInt(Gdx.graphics.getWidth()),
+          r.nextInt(Gdx.graphics.getHeight()),
+          r.nextInt(100), r.nextInt(15), r.nextInt(15)));
+    }
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    shape.begin(ShapeRenderer.ShapeType.Filled);
+    for (Ball ball : balls) {
+      ball.update();
+      ball.draw(shape);
+    }
+    shape.end();
+  }
+}
